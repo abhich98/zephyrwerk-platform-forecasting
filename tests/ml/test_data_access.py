f6fbda9d3f0ff7a85465fbf03f6ae7c5a4a1dfaf
Hasan Erdin <52705602+hasanerdin@users.noreply.github.com>
2026-07-13 14:33:39 +0200

    refactor: move import statement for unittest.mock.patch to the top of the file

--- tests/ml/test_data_access.py
+++ tests/ml/test_data_access.py
@@ -1,10 +1,11 @@
 import logging
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
 
 import ml.data_access as data_access
-from unittest.mock import patch
+
 
 # Mock settings before importing ml.data_access
 @pytest.fixture(scope="session", autouse=True)
